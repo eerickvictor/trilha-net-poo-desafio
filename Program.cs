d986d003c8becd7c7625b0464230fd0a7729f162
Erick Victor <erick.cabral@a.ficr.edu.br>
2024-01-10 23:16:20 -0300

    Implementação concluída

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("1234-5678", "SF-898", "000001" , 7);
+Iphone iphone = new Iphone("8765-4321", "IP03", "0302011", 10);
+
+Console.WriteLine("\nSmarthphone Nokia: \n ");
+    nokia.Ligar();
+    nokia.ReceberLigacao();
+    nokia.InstalarAplicativo("Whatssap");
+Console.WriteLine("\nSmartphone Iphone: \n");
+    iphone.Ligar();
+    iphone.ReceberLigacao();
+    iphone.InstalarAplicativo("Instagram");
